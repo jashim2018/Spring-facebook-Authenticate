8e60a63c62097006fe123d72d511e8c762709d32
sriramede9 <only4programming03@gmail.com>
2020-02-08 19:04:15 -0500

    have a small bug

--- src/main/java/com/sri/springsecurityfacebooklogin/SpringSecurityFacebookLoginApplication.java
+++ src/main/java/com/sri/springsecurityfacebooklogin/SpringSecurityFacebookLoginApplication.java
@@ -2,8 +2,10 @@ package com.sri.springsecurityfacebooklogin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
+@EnableOAuth2Sso
 public class SpringSecurityFacebookLoginApplication {
 
 	public static void main(String[] args) {
